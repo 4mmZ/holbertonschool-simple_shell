e5a31f2a5f831119a1eea2ef9d1106cc9f43983c
NeoDau <neodau@gmail.com>
2022-11-29 09:50:17 -0800

    all

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include "main.h"
-
+/**
+ *
+ *gcc -Wall -Wextra -Werror -pedantic main.c
+ *
+ */
 int main(int ac, char **argv){
 	char *prompt = "andy $ ";
-	char *lineptr;
-	size_t a = 0;
+	char *lineptr = NULL;/*Almacena la direccion del buf*/
+	char *lineptr_copy = NULL;/*Copia de LINEPTR*/
+	size_t a = 0;/*Almacena el tamaño asignado en bytes*/
+	ssize_t chars_read;/*al ser -1 sale de la shell*/
+	const char *elim = " \n";
 
+	/*Variables nulasxd*/
 	(void)ac; (void)argv;
 
-	printf("%s", prompt);
-	getline(&lineptr, &a, stdin);
-	printf("%s\n", lineptr);
+	/*loop infinito*/
+	while(1)
+	{
+		printf("%s", prompt);
+		chars_read = getline(&lineptr, &a, stdin);
+		if (chars_read == -1)
+		{
+			printf("goodbye shell...\n");/**Ctrl D salida shell*/
+			return (-1);
+		}
+		printf("%s\n", lineptr);
+	}
+	/*Liberar memoria asignada*/
 	free(lineptr);
 	return (0);
 }
