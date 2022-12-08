2a9185c9c4fe9bb0e67ce4518b84924aec7bcd67
Andres <andresmaxi111014@gmail.com>
2022-12-08 09:29:49 -0800

    protogetenv

---
+++ andy_stuff/getenvfunct/getenv.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+extern char ** environ;
+  
+char * get_env(char *name)
+{
+	/*/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin*/
+	char 
+	char *path = "";
+	return (path);
+}
+void main(void)
+{
+	printf("pete\n");
+	printf("%s\n", get_env("PATH"));
+}
